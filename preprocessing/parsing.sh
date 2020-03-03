0e5ff14b55f5b056312f87945ae057a7f3899750
DevHyung <khuphj@gmail.com>
2020-03-03 16:41:36 +0900

    Add parsing 예제 shell script

---
+++ preprocessing/parsing.sh
@@ -0,0 +1,8 @@
+PYTHON=/Users/hjpark/Desktop/dev/bin/python3
+${PYTHON} parsing.py \
+  --input_excel sample.xlsx\
+  --output_json result.json\
+  --line_min 7\
+  --summary_min 30\
+  --include_html false\
+
